30ee53760bdffce4a4c797397931f025968a0083
Michael Hobbs <mrmic@example.com>
2025-11-17 07:28:03 +0700

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-﻿import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NET-Ai-EcoSystem-CommandStation/',
+  base: "/NET-Ai-EcoSystem-CommandStation/",
   build: {
-    outDir: 'docs'
-  }
-})
+    outDir: "docs",
+  },
+});
