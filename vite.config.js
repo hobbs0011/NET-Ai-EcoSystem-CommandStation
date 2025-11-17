18020a3b2a398fcb1b01bd24fc80d7dfa1dea82f
Michael Hobbs <mrmic@example.com>
2025-11-17 07:49:16 +0700

    Deploy: React + Vite working build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   base: "/NET-Ai-EcoSystem-CommandStation/",
-  build: { outDir: "docs" }
+  build: {
+    outDir: "docs",
+  },
 });
